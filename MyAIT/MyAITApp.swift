363fe1ec9aba833b6ea2ef16f5111b9cbcb8288f
TasukuFujiwara <tasuku0221@icloud.com>
2023-10-08 15:15:49 +0900

    Initial Commit

---
+++ MyAIT/MyAITApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAITApp.swift
+//  MyAIT
+//
+//  Created by 藤原輔 on 2023/10/08.
+//
+
+import SwiftUI
+
+@main
+struct MyAITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
